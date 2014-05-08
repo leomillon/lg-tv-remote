96081f8086fd9b46aa89540d814e8d1c5d2c3760
Léo Millon <millon.leo@gmail.com>
2014-05-08 19:21:27 +0200

    Keys exported to a module

---
+++ keys.js
@@ -0,0 +1,86 @@
+exports.keys = {
+    POWER: 1,
+    NUMBER: {
+        0: 2,
+        1: 3,
+        2: 4,
+        3: 5,
+        4: 6,
+        5: 7,
+        6: 8,
+        7: 9,
+        8: 10,
+        9: 11
+    },
+    DIRECTION: {
+        UP: 12,
+        DOWN: 13,
+        LEFT: 14,
+        RIGHT: 15
+    },
+    OK: 20,
+    HOME_MENU: 21,
+    MENU_KEY: 22, // same with Home menu key
+    PREVIOUS: 23, // back
+    VOLUME: {
+        UP: 24,
+        DOWN: 25,
+        MUTE: 26
+    },
+    CHANNEL: {
+        UP: 27,
+        DOWN: 28,
+        PREVIOUS: 403, // flash back
+        FAVORITE: 404
+    },
+    COLOR: {
+        BLUE: 29,
+        GREEN: 30,
+        RED: 31,
+        YELLOW: 32
+    },
+    RECORDING: {
+        PLAY: 33,
+        PAUSE: 34,
+        STOP: 35,
+        FAST_FORWARD: 36,
+        REWIND: 37,
+        SKIP_FORWARD: 38,
+        SKIP_BACKWARD: 39,
+        RECORD: 40,
+        LIST: 41
+    },
+    REPEAT: 42,
+    LIVE_TV: 43,
+    EPG: 44,
+    CURRENT_PROG_INFO: 45,
+    ASPECT_RATIO: 46,
+    EXTERNAL_INPUT: 47,
+    SUBTITLE: 49, // Show and change
+    PROG_LIST: 50,
+    TELE_TEXT: 51,
+    MARK: 52,
+    '3D': {
+        VIDEO: 400,
+        LEFT_RIGHT: 401
+    },
+    DASH: 402,
+    QUICK_MENU: 405,
+    TEXT_OPTION: 406,
+    AUDIO_DESCRIPTION: 407,
+    NET_CAST: 408, // same with Home menu
+    ENERGY_SAVING: 409,
+    AV_MODE: 410,
+    SIMPLINK: 411,
+    EXIT: 412,
+    RESERVATION_PROG_LIST: 413,
+    PIP: {
+        SEC_VIDEO: 48,
+        CHANNEL: {
+            UP: 414,
+            DOWN: 415
+        },
+        SWITCH: 416 // Switching between primary/secondary video
+    },
+    MY_APPS: 417
+};
